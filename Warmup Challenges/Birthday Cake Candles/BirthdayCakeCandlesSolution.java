ef6300c64f6d89fa8ca29f5304b78532825e53ec
Krutika Vijay Patil <kpatil3@uncc.edu>
2018-02-11 14:30:15 -0500

    Create BirthdayCakeCandlesSolution.java

---
+++ Warmup Challenges/Birthday Cake Candles/BirthdayCakeCandlesSolution.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int birthdayCakeCandles(int n, int[] ar) {
+        // Complete this function
+        int count = 1;
+        int max = ar[0];
+        for(int i=1;i<n;i++)
+        {
+            if(max==ar[i])
+                count++;
+            else if(max<ar[i])
+            {
+                max=ar[i];
+                count=1;
+            }
+             
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = birthdayCakeCandles(n, ar);
+        System.out.println(result);
+    }
+}
